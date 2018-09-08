6c52bde398b8086ac8b389d3ed48caf659fda84a
uriha421 <uriha421@gmail.com>
2018-09-08 10:08:22 +0900

    add some comments

--- ChapterI/insertionSort/insertionSort.go
+++ ChapterI/insertionSort/insertionSort.go
@@ -12,38 +12,37 @@ import (
 // main() receives command line arguments and returns a sorted array.
 // See insertionSort() for insertion-sort.
 func main() {
-  flag.Parse()
+	flag.Parse()
 
 	var err error
-  argsInt := make([]int, flag.NArg())
+	argsInt := make([]int, flag.NArg())
 
-  for i, v := range flag.Args() {
+	for i, v := range flag.Args() {
 		argsInt[i], err = strconv.Atoi(v)
 		if err != nil {
 			log.Fatal(err)
 		}
 	}
 
-  insertionSort(argsInt)
+	insertionSort(argsInt)
 
-  for _, v := range argsInt {
-    fmt.Printf("%d\n", v)
-  }
+	for _, v := range argsInt {
+		fmt.Printf("%d\n", v)
+	}
 }
 
-
 func insertionSort(a []int) {
-  var k, l int
+	var k, l int
 
-  for i := 1; i < len(a); i++ {
-    // a[:i] has been sorted.
+	for i := 1; i < len(a); i++ {
+		// a[:i] has been sorted.
 
-    k = a[i] // k will be inserted into a[:i], and a[:i+1] will be sorted.
-    l = i - 1 // We will compare k with a[l], (l = i-1, ..., 0).
-    for k < a[l] && l >= 0 {
-      a[l + 1] = a[l]
-      a[l] = k
-      l--
-    }
-  }
+		k = a[i]  // k will be inserted into a[:i], and a[:i+1] will be sorted.
+		l = i - 1 // We will compare k with a[l], (l = i-1, ..., 0).
+		for k < a[l] && l >= 0 {
+			a[l+1] = a[l]
+			a[l] = k
+			l--
+		}
+	}
 }
