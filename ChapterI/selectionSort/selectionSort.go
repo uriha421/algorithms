c544b60d1b317899c11313dbea5e4dfe8da3e06f
uriha421 <uriha421@gmail.com>
2018-09-09 17:21:31 +0900

    go fmt

--- ChapterI/selectionSort/selectionSort.go
+++ ChapterI/selectionSort/selectionSort.go
@@ -12,43 +12,42 @@ import (
 // main() receives command line arguments and returns a sorted array.
 // See selectionSort() for selection-sort.
 func main() {
-  flag.Parse()
+	flag.Parse()
 
 	var err error
-  argsInt := make([]int, flag.NArg())
+	argsInt := make([]int, flag.NArg())
 
-  for i, v := range flag.Args() {
+	for i, v := range flag.Args() {
 		argsInt[i], err = strconv.Atoi(v)
 		if err != nil {
 			log.Fatal(err)
 		}
 	}
 
-  selectionSort(argsInt)
+	selectionSort(argsInt)
 
-  for _, v := range argsInt {
-    fmt.Printf("%d\n", v)
-  }
+	for _, v := range argsInt {
+		fmt.Printf("%d\n", v)
+	}
 }
 
-
 func selectionSort(a []int) {
-  var k, l int
-  for i := 0; i < len(a) - 1; i++ {
+	var k, l int
+	for i := 0; i < len(a)-1; i++ {
 
-    // k will be the least number in a[i:len(a)].
-    // l will be the index of the least number in a[i:len(a)].
+		// k will be the least number in a[i:len(a)].
+		// l will be the index of the least number in a[i:len(a)].
 		k = a[i]
 		l = i
-    for j := i + 1; j < len(a); j++ {
-      if (k > a[j]) {
-        k = a[j]
-        l = j
-      }
-    }
+		for j := i + 1; j < len(a); j++ {
+			if k > a[j] {
+				k = a[j]
+				l = j
+			}
+		}
 
-    // a[0:i] has been sorted.
-    a[l] = a[i]
-    a[i] = k
-  }
+		// a[0:i] has been sorted.
+		a[l] = a[i]
+		a[i] = k
+	}
 }
